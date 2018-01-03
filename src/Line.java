0312d37f6b691fbaf1b19d72ec232f1b97ddf99d
Cochine <34948866+Cochine@users.noreply.github.com>
2018-01-02 19:23:28 -0500

    Test commit.

--- src/Line.java
+++ src/Line.java
@@ -3,10 +3,12 @@ import java.awt.Point;
 
 public class Line extends Piece {
 
-	static Point[] points = {new Point(-1,0), new Point(0,0), new Point(1,0), new Point(2,0)};
+	static Point[] points = {};
 	
 	public Line() {
 		super (Color.cyan, points);
+		Point[] points = {new Point(-1,0), new Point(0,0), new Point(1,0), new Point(2,0)};
+		this.setPoints(points);
 		
 	}
 	
--- src/Piece.java
+++ src/Piece.java
@@ -6,6 +6,10 @@ public abstract class Piece {
 	private Color color;
 	private Point points[];
 
+	public void setPoints(Point[] points) {
+		this.points = points;
+	}
+
 	public Piece(Color color, Point[] points) {
 		this.color = color;
 		this.points = points;
